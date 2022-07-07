b57593193754930ae2fc417fa01a2d62d8560eac
Anna_Koreva <koreva.anna@bk.ru>
2022-07-07 22:26:01 +0300

    username

---
+++ namepolz/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "mars")
+{
+   Console.WriteLine("УРА ЭТО ЖЕ MARS"); 
+}
+else
+{
+    Console.Write("привет, ");
+    Console.WriteLine(username);
+}
